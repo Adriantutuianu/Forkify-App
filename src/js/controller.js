43ab44bf4b40aa5e7fe503b61e8acfcaea6f5754
Adrian <adriantutuianu01@gmail.com>
2021-09-08 22:08:24 +0100

    Listening for load an hashchange event

--- src/js/controller.js
+++ src/js/controller.js
@@ -34,6 +34,8 @@ const showRecipe = async function () {
   try {
     const id = window.location.hash.slice(1);
     console.log(id);
+
+    if (!id) return;
     //Loading recipe
     renderSpinner(recipeContainer);
 
