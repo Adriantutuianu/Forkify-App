6b72792090d696f602dcc4af9be29cb9c6f003e7
Adrian <adriantutuianu01@gmail.com>
2021-09-08 22:02:08 +0100

    For load Event

--- src/js/controller.js
+++ src/js/controller.js
@@ -32,12 +32,13 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
     //Loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca3b'
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
