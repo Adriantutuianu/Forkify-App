434332c320e3bd3df6f04bb852e019b909b966ba
Adrian <adriantutuianu01@gmail.com>
2021-09-16 23:29:10 +0100

    Created paginationView.js

---
+++ src/js/views/paginationView.js
@@ -0,0 +1,6 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg'; //parcel 2
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+}
