f37aaca38dbfe35d7e1b342e3e05c9216ee77f67
Adrian <adriantutuianu01@gmail.com>
2021-09-24 22:12:59 +0100

    Constructor method

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -9,7 +9,12 @@ class AddRecipeView extends View {
   _btnOpen = document.querySelector('.nav__btn--add-recipe');
   _btnClose = document.querySelector('.nav__btn--close-modal');
 
-  addHandlerShowWindow() {
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+  }
+
+  _addHandlerShowWindow() {
     this._btnOpen.addEventListener('click', function () {
       this._overlay.classList.toggle('hidden');
       this._window.classList.toggle('hidden');
